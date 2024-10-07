da4ce5450d834cd1f0c6a0bd2e0a2e44d96d7b28
EM.BOUTAHIR <em.boutahir.ext@bouygues-construction.com>
2024-10-08 00:48:24 +0200

    Revert "initial commit"
    This reverts commit 1c51adace2f563b3322857b807b46f6d74d13c2d.

--- backend/src/main/java/com/alten/backend/exceptions/FunctionalException.java
+++
@@ -1,7 +0,0 @@
-package com.alten.backend.exceptions;
-
-public class FunctionalException extends RuntimeException {
-    public FunctionalException(String message) {
-        super(message);
-    }
-}
